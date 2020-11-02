7e527d878110bd9102ecb226ea9ba6d925f6f33d
Sondre Hamnvik <sondrehamnvik@gmail.com>
2020-11-02 18:22:19 +0100

    Updated setup.pu

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='yolo_apnea_predicter',
     version='0.0.1',
-    packages=['yolo_apnea_predicter'],
+    packages=find_packages('yolo_apnea_predicter'),
+    package_dir={'':'yolo_apnea_predicter'},
     description='An interface for accessing predictions of apneas using abdo or thor sensors and yolo'
 
 )
