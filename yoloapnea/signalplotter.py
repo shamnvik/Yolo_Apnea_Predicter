7f572cf264bbcc6ea44f5625e99e4bd72b31a813
Sondre Hamnvik <sondrehamnvik@gmail.com>
2021-02-26 15:08:06 +0100

    Created standalone image plotter

---
+++ yoloapnea/signalplotter.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+class SignalPlotter:
+
+    def __init__(self):
+        print("init signal plotter")
+        self.image_duration = 900 #TODO placeholder,add to constructor
+        self.sliding_window_overlap = 450 #TODO placeholder, add to constructor
+
+    def plot_signal(self,signal):
+        remaining_signal = len(signal)
+        start_index = 0
+
+        if remaining_signal < self.image_duration:
+            raise Exception("Signal length is too short")
+
+        while remaining_signal > 0:
+
+            if remaining_signal == self.image_duration:
+                yield start_index,self.signal_to_image(signal)
+                remaining_signal= 0
+
+            elif remaining_signal > self.image_duration:
+                yield start_index,self.signal_to_image(signal[start_index:start_index + self.image_duration])
+                start_index = start_index + self.sliding_window_overlap
+                remaining_signal -= self.sliding_window_overlap
+
+            elif remaining_signal < self.image_duration:
+                start_index = len(signal)-self.image_duration
+                yield start_index, self.signal_to_image(signal[-self.image_duration:])
+                remaining_signal = 0
+
+
+
+    def signal_to_image(self,signal):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.plot(signal)
+        ax.set_ylim(-1, 1)
+
+        fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        ax.grid(False)
+        plt.axis('off')
+        ax.set_xlim(0, 900)
+
+        fig.canvas.draw()
+        img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        plt.close(fig)
+
+        return img
