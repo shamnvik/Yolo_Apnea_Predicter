feb98e37754e7109f4f7b857f3b2059b9f6671fe
Sondre Hamnvik <sondrehamnvik@gmail.com>
2021-03-10 21:31:46 +0100

    Added tests

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -3,28 +3,25 @@ import yoloapnea.evaluate as evaluate
 from yoloapnea.apneas import ApneaType
 
 import numpy as np
-
+from sklearn.metrics import f1_score
 
 class TestEvaluate(TestCase):
     
     def setUp(self):
         apnea_types = [ApneaType.ObstructiveApnea]
-        pred = np.array([0,1,0,0])
-        truth = np.array([0,1,0,0])
-        self.evaluate = evaluate.Evaluate(pred,truth,apnea_types)
-        
-    def test_metrics(self):
-        ...
-        #self.evaluate.metrics
+        self.pred  = np.array([0,1,0,0,0,1,1,0])
+        self.truth = np.array([0,1,0,0,0,1,0,0])
+        self.evaluate = evaluate.Evaluate(self.pred,self.truth,apnea_types)
 
     def test_f1(self):
-         value = self.evaluate.f1
+        value = self.evaluate.f1
+        self.assertEqual(value,f1_score(y_true=self.truth,y_pred=self.pred))
 
     def test_predictedAHI(self):
         value = self.evaluate.predictionAHI
-        print(value)
+        self.assertGreaterEqual(value,0)
 
     def test_groundTruth_AHI(self):
         value = self.evaluate.groundTruthAHI
-        print(value)
+        self.assertGreaterEqual(value,0)
 
