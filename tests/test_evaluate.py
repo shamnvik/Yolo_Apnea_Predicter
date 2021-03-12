4b6e302b790f721e45a2028262e83ca52b295748
Sondre Hamnvik <sondrehamnvik@gmail.com>
2021-03-12 19:18:33 +0100

    Added scores to evaluate.py

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,27 +1,32 @@
 from unittest import TestCase
+
 import yoloapnea.evaluate as evaluate
 from yoloapnea.apneas import ApneaType
 
 import numpy as np
 from sklearn.metrics import f1_score
 
+
 class TestEvaluate(TestCase):
-    
+
     def setUp(self):
         apnea_types = [ApneaType.Obstructive]
-        self.pred  = np.array([0,1,0,0,0,1,1,0])
-        self.truth = np.array([0,1,0,0,0,1,0,0])
-        self.evaluate = evaluate.Evaluate(self.pred,self.truth,apnea_types)
+        self.pred = np.array([0, 0.3, 0, 0, 0, 0.3, 1, 0])
+        self.truth = np.array([2, 1, 0, 0, 0, 1, 0, 0])
+        self.evaluate = evaluate.Evaluate(self.pred, self.truth, apnea_types,0)
 
     def test_f1(self):
         value = self.evaluate.f1
-        self.assertEqual(value,f1_score(y_true=self.truth,y_pred=self.pred))
+        self.assertEqual(value, f1_score(y_true=self.truth, y_pred=self.pred))
 
     def test_predictedAHI(self):
         value = self.evaluate.predictionAHI
-        self.assertGreaterEqual(value,0)
+        self.assertGreaterEqual(value, 0)
 
     def test_groundTruth_AHI(self):
         value = self.evaluate.groundTruthAHI
-        self.assertGreaterEqual(value,0)
+        self.assertGreaterEqual(value, 0)
 
+    def test_scores(self):
+        scores = self.evaluate.scores
+        print(scores)
--- yoloapnea/evaluate.py
+++ yoloapnea/evaluate.py
@@ -1,23 +1,83 @@
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score,recall_score
+from sklearn.metrics import confusion_matrix,roc_curve,auc,accuracy_score,precision_score
+
+import matplotlib.pyplot as plt
+
 from .apneas import ApneaType
 import numpy as np
 import pandas as pd
 
 class Evaluate:
 
-    def __init__(self,predictions,ground_truth,apnea_types):
-        print("init evaluate")
+    def __init__(self,predictions,ground_truth,apnea_types,threshold):
         self.predictions = predictions
-        self.predictionsBool = predictions>0
+        self.predictionsBool = predictions>threshold
         self.ground_truth = np.isin(ground_truth,[apnea.value for apnea in apnea_types])
+        self.threshold = 0.25
 
     @property
-    def metrics(self):
-        raise NotImplementedError("evaluate.metrics not implemented")
+    def scores(self):
+        return {
+            "f1": self.f1,
+            "confusion_matrix": self.confusion_matrix,
+            "roc_curve" : self.roc_curve,
+            "auc": self.auc,
+            "accuracy": self.accuracy,
+            "ahi":self.ahi,
+            "precision":self.precision,
+            "recall":self.recall
+        }
 
     @property
     def f1(self):
         return f1_score(self.ground_truth,self.predictionsBool)
+
+    @property
+    def confusion_matrix(self):
+
+        tn, fp, fn, tp = confusion_matrix(self.ground_truth,self.predictionsBool).ravel()
+        return {
+            "tn":tn,
+            "fp":fp,
+            "fn":fn,
+            "tp":tp
+        }
+
+    @property
+    def roc_curve(self):
+        fpr, tpr, thresholds = roc_curve(self.ground_truth, self.predictions,pos_label=1)
+        return {
+            "fpr":fpr,
+            "tpr":tpr,
+            "tresholds":thresholds
+        }
+
+    @property
+    def auc(self):
+        roc_curve = self.roc_curve
+        fpr = roc_curve["fpr"]
+        tpr = roc_curve["tpr"]
+        return auc(fpr,tpr)
+
+
+    @property
+    def accuracy(self):
+        return accuracy_score(y_true=self.ground_truth,y_pred=self.predictionsBool)
+
+    @property
+    def ahi(self):
+        return {
+            "prediction_AHI": self.predictionAHI,
+            "true_AHI":self.groundTruthAHI
+        }
+
+    @property
+    def precision(self):
+        return precision_score(self.ground_truth,self.predictionsBool)
+
+    @property
+    def recall(self):
+        return recall_score(self.ground_truth,self.predictionsBool)
 
     @property
     def predictionAHI(self):
